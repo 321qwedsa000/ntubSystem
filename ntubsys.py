ece96295b74730937522e8faaddf699d74212e6e
TecHaonical <39931469+321qwedsa000@users.noreply.github.com>
2020-12-24 13:18:35 +0800

    Update ntubsys.py

--- ntubsys.py
+++ ntubsys.py
@@ -19,6 +19,7 @@ class NtubLoginSystem:
         self.session = requests.Session()
         self.LOGIN_URL = "http://ntcbadm1.ntub.edu.tw/login.aspx?Type=0" #POST url
         self.CURRICULUM_URL = "http://ntcbadm1.ntub.edu.tw/STDWEB/Sel_Student.aspx" #GET url
+        self.MIDTERM_URL = "http://ntcbadm1.ntub.edu.tw/ACAD/STDWEB/GRD_GRDMQry.aspx"
         self.username = username
         self.password = password
         loginData = {
@@ -52,17 +53,33 @@ class NtubLoginSystem:
                     lesson = a_tag.text
                     teacher = col.text[len(lesson):-4]
                     classroom = col.text[len(lesson)+len(teacher):]
-                    column.append(f'{lesson}\n{teacher}\n{classroom}')
+                    column.append(f'{lesson}\n{teacher}\n{classroom}') #split it
                 else:
-                    replaceStr = re.sub(r'\d\d:\d\d\d\d:\d\d','',col.text)
+                    replaceStr = re.sub(r'\d\d:\d\d\d\d:\d\d','',col.text) #Remove duplicate stuffs
                     column.append(replaceStr)
             table.append(column)
         return table
-        
-        
+    
+    def search_midterm_score(self,seayear,seaterm):
+        search_dict = {
+            'SEA_Year':seayear,
+            'SEA_Term':seaterm
+        }
+        self.__search_VIEW(self.MIDTERM_URL,search_dict)
+        response = self.session.get(self.MIDTERM_URL,data=search_dict,cookies=self.cookies)
+        try:
+            soup = BeautifulSoup(response.text,'lxml')
+        except:
+            soup = BeautifulSoup(response.text,'html.parser')
+        score_dict = {}
+        itemTable = soup.findAll('td',attrs={'width':'380'})
+        scoreTable = soup.findAll('span',attrs={'id':lambda a: a and a[-1] == "M"})
+        for i in range(len(itemTable)):
+            score_dict[itemTable[i].text] = float(scoreTable[i].text.replace('*','') if scoreTable[i].text != "" else "0.00")
+        return score_dict
 
 if __name__ == "__main__":
     import getpass
     import pprint
-    ntubLogin = NtubLoginSystem(input(),getpass.getpass())
-    pprint.pprint(ntubLogin.search_curriculum(108,2))
+    ntubLogin = NtubLoginSystem(input('User Name:'),getpass.getpass())
+    pprint.pprint(ntubLogin.search_midterm_score(109,1))
